962d43a5bc35926e5c0b34040c27f6e9d742070c
JoaoPEDRO <joaopedrocorrea160@gmail.com>
2022-11-18 19:34:27 -0300

    Código Cru

---
+++ src/main/java/Revisao/RevisaoElProva/RevisaoElProvaApplication.java
@@ -0,0 +1,13 @@
+package Revisao.RevisaoElProva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisaoElProvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisaoElProvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Revisao/RevisaoElProva/RevisaoElProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package Revisao.RevisaoElProva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisaoElProvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
